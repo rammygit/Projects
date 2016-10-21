1f76f0ba565d24ff223969084f784f48b2d9296f
ramk <rammyramkumar@gmail.com>
2016-10-21 08:33:17 -0400

    added external configuration.

--- MainApplication/src/main/java/app/verticle/MyFirstVerticle.java
+++ MainApplication/src/main/java/app/verticle/MyFirstVerticle.java
@@ -16,7 +16,10 @@ public class MyFirstVerticle extends AbstractVerticle{
 		HttpServer httpServer = vertx.createHttpServer();
 		httpServer.requestHandler(r->r.response().end("<h1>Hello from my first " +
               "Vert.x 3 application</h1>"));
-		httpServer.listen(8080,result->{
+		httpServer.listen(
+				 // Retrieve the port from the configuration,
+		          // default to 8080.
+		          config().getInteger("http.port", 8080),result->{
 			if (result.succeeded()) {
 	            future.complete();
 	          } else {
