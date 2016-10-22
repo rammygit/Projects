9152e9515a93b936ebcd861c66f84aad9af5b210
ramk <rammyramkumar@gmail.com>
2016-10-22 00:10:44 -0400

    added vertx-web and added router

--- MainApplication/src/main/java/app/verticle/MyFirstVerticle.java
+++ MainApplication/src/main/java/app/verticle/MyFirstVerticle.java
@@ -3,6 +3,8 @@ package app.verticle;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 /**
  * my first verticle.
@@ -13,9 +15,19 @@ public class MyFirstVerticle extends AbstractVerticle{
 	
 	public void start(Future<Void> future){
 		
+		/**
+		 * router config. 
+		 */
+		Router router = Router.router(vertx);
+		router.route("/").handler(routingContext ->{
+			
+			HttpServerResponse response = routingContext.response();
+			response.putHeader("content-type", "text/html")
+		       .end("<h1>Hello from my first Vert.x 3 application</h1>");
+		});
+		
 		HttpServer httpServer = vertx.createHttpServer();
-		httpServer.requestHandler(r->r.response().end("<h1>Hello from my first " +
-              "Vert.x 3 application</h1>"));
+		httpServer.requestHandler(router::accept);
 		httpServer.listen(
 				 // Retrieve the port from the configuration,
 		          // default to 8080.
